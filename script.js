9dafcdb2505a3bb3385b3699ba5b864b4753a937
GuilleSierra11 <guillesect@gmail.com>
2026-03-07 22:39:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,114 @@
 const cartas = [
     {
-        imagen: "/spartak_cup/Cartas/bidoff.png",
+        imagen: "cartas/bidoff.png",
         precio: 50,
         duracion: "3 horas"
     },
     {
-        imagen: "Cartas/cambio.png",
-        precio: 30,
+        imagen: "cartas/cambio.png",
+        precio: 80,
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/candado.png",
-        precio: 30,
+        imagen: "cartas/candado.png",
+        precio: 120,
         duracion: "1 hora y 30 minutos"
     },
     {
-        imagen: "Cartas/cap.png",
-        precio: 35,
+        imagen: "cartas/cap.png",
+        precio: 100,
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/copia.png",
-        precio: 55,
+        imagen: "cartas/copia.png",
+        precio: 80,
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/especial.png",
-        precio: 40,
-        duracion: "3 horas"
+        imagen: "cartas/especial.png",
+        precio: 120,
+        duracion: "5 horas"
     },
     {
-        imagen: "Cartas/hembras.png",
-        precio: 40,
-        duracion: "2 horas"
-    },
-    {
-        imagen: "Cartas/karateka.png",
-        precio: 40,
-        duracion: "3 horas"
-    },
-    {
-        imagen: "Cartas/la_llamada.png",
-        precio: 75,
-        duracion: "Al instante"
-    },
-    {
-        imagen: "Cartas/la_parca.png",
-        precio: 75,
-        duracion: "Al instante"
-    },
-    {
-        imagen: "Cartas/machos.png",
-        precio: 40,
-        duracion: "2 horas"
-    },
-    {
-        imagen: "Cartas/monotype.png",
+        imagen: "cartas/hembras.png",
         precio: 50,
         duracion: "2 horas"
     },
     {
-        imagen: "Cartas/ruleta.png",
-        precio: 15,
+        imagen: "cartas/karateka.png",
+        precio: 80,
+        duracion: "3 horas"
+    },
+    {
+        imagen: "cartas/la_llamada.png",
+        precio: 120,
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/tr.png",
-        precio: 60,
+        imagen: "cartas/la_parca.png",
+        precio: 50,
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/vetado.png",
-        precio: 15,
+        imagen: "cartas/machos.png",
+        precio: 80,
+        duracion: "2 horas"
+    },
+    {
+        imagen: "cartas/monotype.png",
+        precio: 120,
+        duracion: "2 horas"
+    },
+    {
+        imagen: "cartas/ruleta.png",
+        precio: 50,
+        duracion: "Al instante"
+    },
+    {
+        imagen: "cartas/TR.png",
+        precio: 80,
+        duracion: "Al instante"
+    },
+    {
+        imagen: "cartas/vetado.png",
+        precio: 120,
         duracion: "1 hora"
     },
     {
-        imagen: "Cartas/doble_o_nada.png",
-        precio: A tu elección,
+        imagen: "cartas/doble_o_nada.png",
+        precio: "A tu elección",
         duracion: "Al instante"
     },
     {
-        imagen: "Cartas/desarme.png",
+        imagen: "cartas/desarme.png",
         precio: 40,
-        duracion: "3 hora"
+        duracion: "3 horas"
     }
 ]
 
 const contenedor = document.getElementById("cards")
 
 cartas.forEach(carta => {
+
+    const precioHTML = typeof carta.precio === "number"
+        ? `Precio ${carta.precio} <span class="moneda">$</span>`
+        : `Precio ${carta.precio}`
+
     const div = document.createElement("div")
     div.className = "card"
 
     div.innerHTML = `
-    <img src="${carta.imagen}">
-    <div class="info">
-        <div class="precio">
-            Precio ${carta.precio}
-            <span class="moneda">$</span>
+        <img src="${carta.imagen}">
+        <div class="info">
+            <div class="precio">
+                ${precioHTML}
+            </div>
+            <div class="duracion">
+                Duración: ${carta.duracion}
+            </div>
+            <button class="boton-ver">Ver carta</button>
         </div>
-        <div class="duracion">
-            Duración: ${carta.duracion} 
-        </div>
-        <button class="boton-ver">Ver carta</button>
-    </div>
-`
+    `
 
     contenedor.appendChild(div)
 })
@@ -114,11 +118,17 @@ const imagenGrande = document.getElementById("imagen-grande")
 const cerrar = document.querySelector(".cerrar")
 
 document.addEventListener("click", function(e) {
+
     if (e.target.classList.contains("boton-ver")) {
-        const imagen = e.target.closest(".card").querySelector("img").src
+
+        const imagen = e.target
+            .closest(".card")
+            .querySelector("img").src
+
         imagenGrande.src = imagen
         modal.style.display = "block"
     }
+
 })
 
 cerrar.onclick = function() {
@@ -129,14 +139,4 @@ modal.onclick = function(e) {
     if (e.target === modal) {
         modal.style.display = "none"
     }
-
 }
-
-
-
-
-
-
-
-
-
